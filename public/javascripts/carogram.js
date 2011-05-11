6bf933c66cfea7992ac28a7e4365d9756b02a9f7
Adam McDonald <mcdonald.adam@gmail.com>
2011-05-10 22:49:04 -0700

    First two IG photos are now pulled from feed.

--- public/javascripts/carogram.js
+++ public/javascripts/carogram.js
@@ -3,8 +3,12 @@ var WIDTH_BOUNDARY = 1460;
 var BACK_LEFT_DEFAULT = 40;
 var FORWARD_LEFT_DEFAULT = 1020;
 var FORWARD_RIGHT_DEFAULT = 40;
-
 var PHOTO_LEFT_BOUNDARY = 480;
+
+var KEYCODE_LEFT_ARROW = 37;
+var KEYCODE_RIGHT_ARROW = 39;
+
+var igSelfFeed = null;
 
 $(document).ready(function() {
   // Initial layout adjustment
@@ -26,20 +30,61 @@ $(document).ready(function() {
   
   console.log('made it. must be authenticated');
   
+  // Resize events
   $(window).resize(function(e) {
     adjustLayout();
   });
   
-  /*
+  // Right arrow events
+  $(document).bind('keydown', function(e) {
+    switch(e.which) {
+      case KEYCODE_LEFT_ARROW:
+        e.preventDefault();
+        previousPhoto();
+        break;
+      case KEYCODE_RIGHT_ARROW:
+        e.preventDefault();
+        nextPhoto();
+        break;
+    }
+  });
+  
   $.get('https://api.instagram.com/v1/users/self/feed', { 'access_token': IG._session.access_token, 'callback': 'handleFeed' }, function(data) {
     //var json = $.parseJSON(data);
     //console.log(json);
   }, 'script');
-  */
 });
 
 function handleFeed(feed) {
-  console.log(feed.data.length);
+  igSelfFeed = feed;
+  
+  console.log(igSelfFeed.data.length);
+  
+  var first = igSelfFeed.data[0];
+  var firstURL = first.images.standard_resolution.url;
+  $('#photo > img').attr('src', firstURL);
+  
+  var second = igSelfFeed.data[1];
+  var secondURL = second.images.standard_resolution.url;
+  $('#forward-image > img').attr('src', secondURL);
+}
+
+function nextPhoto() {
+  console.log('next');
+  //$('#content').effect('drop', 500);
+  $('#wrapper').animate({
+    opacity: 0.25,
+    marginLeft: 0
+  }, 1500);
+}
+
+function previousPhoto() {
+  console.log('prev');
+  
+  $('#wrapper').animate({
+    opacity: 0.25,
+    marginRight: 0
+  }, 1500);
 }
 
 function adjustLayout() {
